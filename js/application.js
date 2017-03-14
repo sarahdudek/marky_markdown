6f7d7ec9cdbce7bde10dca4f08a6207cefd847d9
Al Musavi & Sarah Dudek <pair+almusavi.sarahdudek@devbootcamp.com>
2017-03-14 14:23:19 -0500

    Make it work. Yeahhhhhh

--- js/application.js
+++ js/application.js
@@ -1,23 +1,55 @@
 // view for boxes
 // rawSide -->
 // mdSide
-
-function transform(input) {
-  return input
-}
+console.log("hi")
  // bind something to input area keyup
  // function to check the text markdownability (AJAX stuff)
-
+// for italics (\*|\_)([^\*]*)(\*|\_|\z)
 // specs: *, _, **
 
-function mdChecker(textInput) {
-  if (){
+// function mdChecker(textInput) {
+//   if (){
 
-  } else if (){
+//   } else if (){
 
-  } else if(){
+//   } else if(){
 
-  }
+//   }
+// }
+
+$(document).ready(function(){
+  init();
+});
+
+function init(){
+  $(".input").on("keyup", runMD);
+}
+
+// for bold /(\*\*)([^\*]*)(\*\*|\z)
+
+function runMD(e){
+  e.preventDefault();
+  var rawInput = $(this).val();
+  var input = transform(rawInput);
+  $(".output").html(input);
+}
+
+function transform(input) {
+  var myBoldRegexp = /(\*\*)([^\*]*)(\*\*)/g;
+  var myUnderlineRegexp = /(\_)([^\*]*)(\_|\z)/g;
+  var myItalicsRegexp = /(\*)([^\*]*)(\*|\z)/g;
+  // var match = input.match(myBoldRegexp)
+  //     // var match = myRegexp.exec(input);
+  //     debugger
+  // var newstr = input.replace(match, "!")
+  // return newstr;
+
+  var firstComparison = input.replace(myBoldRegexp, "<b>$2</b>");
+  var secondComparison = firstComparison.replace(myUnderlineRegexp, "<u>$2</u>");
+  var newStr = secondComparison.replace(myItalicsRegexp, "<i>$2</i>");
+
+  return newStr
 }
 
 
+// Hello *Al* nice day, **right**? _poop_
