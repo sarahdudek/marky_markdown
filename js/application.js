cf5449a2ce7f81171ddc3e4972c15e86ccb78a9d
Al Musavi & Sarah Dudek <pair+almusavi.sarahdudek@devbootcamp.com>
2017-03-14 10:33:11 -0500

    add html

---
+++ js/application.js
@@ -0,0 +1,23 @@
+// view for boxes
+// rawSide -->
+// mdSide
+
+function transform(input) {
+  return input
+}
+ // bind something to input area keyup
+ // function to check the text markdownability (AJAX stuff)
+
+// specs: *, _, **
+
+function mdChecker(textInput) {
+  if (){
+
+  } else if (){
+
+  } else if(){
+
+  }
+}
+
+
